2a2d1b5df770a83043dee0b008ebd0f9c4301110
Desmond Chi <dcchi@live.com>
2022-09-07 09:39:22 -0700

    Changed player name color in list and chat.

--- src/me/desmondcchi/enhanced_chat/listeners/PlayerChat.java
+++ src/me/desmondcchi/enhanced_chat/listeners/PlayerChat.java
@@ -1,14 +1,24 @@
 package me.desmondcchi.enhanced_chat.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 import net.md_5.bungee.api.ChatColor;
 
 public class PlayerChat implements Listener {
 	@EventHandler
 	public void onPlayerChat(AsyncPlayerChatEvent event) {
-		event.setFormat(ChatColor.AQUA + "%s" + ChatColor.GRAY + ": " + ChatColor.WHITE + "%s");
+		event.setFormat("%s" + ChatColor.GRAY + ": " + ChatColor.WHITE + "%s");
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+		
+		player.setDisplayName(ChatColor.DARK_PURPLE + player.getName());
+		player.setPlayerListName(ChatColor.DARK_PURPLE + player.getName());
 	}
 }
