bc9672b483bd8cc326c40d2e6ed8e6a4ed5c59e4
Desmond Chi <dcchi@live.com>
2022-09-07 09:16:46 -0700

    Project setup.

---
+++ src/me/desmondcchi/enhanced_chat/listeners/PlayerChat.java
@@ -0,0 +1,11 @@
+package me.desmondcchi.enhanced_chat.listeners;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class PlayerChat implements Listener {
+	@Override
+	public void onPlayerChat(AsyncPlayerChatEvent event) {
+		
+	}
+}
