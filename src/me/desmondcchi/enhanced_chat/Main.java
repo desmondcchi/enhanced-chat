fe0f4643873bcefa7d0af7dbb1c468cb0e79044f
Desmond Chi <dcchi@live.com>
2022-09-08 13:06:49 -0700

    Added custom join and leave messages.

--- src/me/desmondcchi/enhanced_chat/Main.java
+++ src/me/desmondcchi/enhanced_chat/Main.java
@@ -4,12 +4,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import me.desmondcchi.enhanced_chat.commands.Nickname;
 import me.desmondcchi.enhanced_chat.listeners.PlayerChat;
+import me.desmondcchi.enhanced_chat.listeners.PlayerJoinAndLeave;
 
 public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
+		// Commands
 		this.getCommand("nickname").setExecutor(new Nickname());
+		
+		// Listeners
 		this.getServer().getPluginManager().registerEvents(new PlayerChat(), this);
+		this.getServer().getPluginManager().registerEvents(new PlayerJoinAndLeave(), this);
 	}
 	
 	@Override
